baec613f1e202ec4febd904c723cd361c969dc49
Asifur Rahman <asifurrahmanpial@gmail.com>
2020-09-26 02:56:13 +0600

    finished site

---
+++ src/fakeData/index.js
@@ -0,0 +1,31 @@
+const fakeData = [
+	{
+		title: "Cox's Bazar",
+		desc:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ullam excepturi voluptatum iure quae est delectus ipsum",
+		longDesc:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ullam excepturi voluptatum iure quae est delectus ipsum? Numquam voluptatem id nesciunt, impedit autem nulla aspernatur quod labore voluptatibus optio dolore, facere corporis incidunt amet officiis harum eum ratione alias tempora! Incidunt, voluptates sequi vel a fugit, sint saepe commodi iusto corrupti est minus laborum nesciunt sapiente sed necessitatibus",
+		id: "item-1",
+		imgUrl: "https://i.ibb.co/VmxzVDr/Sajek.png",
+	},
+	{
+		title: "Sreemangal",
+		desc:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ullam excepturi voluptatum iure quae est delectus ipsum? ",
+		longDesc:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ullam excepturi voluptatum iure quae est delectus ipsum? Numquam voluptatem id nesciunt, impedit autem nulla aspernatur quod labore voluptatibus optio dolore, facere corporis incidunt amet officiis harum eum ratione alias tempora! Incidunt, voluptates sequi vel a fugit, sint saepe commodi iusto corrupti est minus laborum nesciunt sapiente sed necessitatibus",
+		id: "item-2",
+		imgUrl: "https://i.ibb.co/NKvzGR3/Sreemongol.png",
+	},
+	{
+		title: "Sundarbans",
+		desc:
+			"Accusantium laudantium nemo repellendus unde? Atque, perferendis dolore? Labore veniam atque possimus natus consectetur neque similique. Assumenda laboriosam earum officia",
+		longDesc:
+			"Accusantium laudantium nemo repellendus unde? Atque, perferendis dolore? Labore veniam atque possimus natus consectetur neque similique. Assumenda laboriosam earum officia delectus perspiciatis consequuntur, praesentium doloremque aspernatur, dicta expedita autem modi quae dolor eaque fugiat iure! Nobis provident quae fuga obcaecati qui quis laborum, sequi doloremque amet quibusdam",
+		id: "item-3",
+		imgUrl: "https://i.ibb.co/qFd2kpr/Sundorbon.png",
+	},
+];
+
+export default fakeData;
